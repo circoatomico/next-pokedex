2b02756d968274efd17bd03744ffc609318bc231
Luiz <luiz.gomes@distrito.me>
2022-08-23 01:19:31 -0300

    Added Pokemon details page

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({children}) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="shortcut icon" href="../images/favicon.ico" />
         <title>PokeNext</title>
       </Head>
 
